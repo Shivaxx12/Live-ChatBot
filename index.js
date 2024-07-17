dbb99a7a8e1b5b6972ffd65848bf3f5a1b45c35f
Shivaksh Sharma <100510159+Shivaxx12@users.noreply.github.com>
2024-07-18 01:44:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try{
+    const r= await axios.put('https://api.chatengine.io/users/',
+    { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "a1929f93-f35b-4e90-8ab5-26f58bf228e9" } }
+  );
+  return res.status(r.status).json(r.data);
+  }
+  catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+app.listen(3001);
